090e63ee120b2bafb332401f31f001de6444a97e
Lionel <lio2014vt@gmail.com>
2025-02-19 09:43:27 -0300

    Fin sección 13: PC-OFICINA

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -67,6 +67,13 @@ export class AuthService {
 
   }
 
+  async checkAuthStatus( user: User ) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id })
+    }
+  }
+
   private getJwtToken( payload: JwtPayload  ) {
 
       console.log('Payload para el token:', payload);

01116b628a906c695e98350a02d9abbb43823bfd
lionel <lio2014vt@gmail.com>
2025-02-18 23:26:10 -0300

    Updated: SEED de usuarios, productos e imágenes, clase 13: PC

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -8,6 +8,7 @@ import { PaginationDto } from '../common/dtos/pagination.dto';
 
 import { validate as isUUID } from 'uuid';
 import { ProductImage, Product } from './entities';
+import { User } from '../auth/entities/user.entity';
 
 @Injectable()
 export class ProductsService {
@@ -26,7 +27,7 @@ export class ProductsService {
 
   ) {}
 
-  async create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto, user: User) {
 
     try {
 
@@ -34,7 +35,8 @@ export class ProductsService {
 
       const product = this.productRepository.create({
         ...productDetails,
-        images: images.map( image =>  this.productImageRepository.create({ url: image }))
+        images: images.map( image =>  this.productImageRepository.create({ url: image })),
+        user
       });
 
       await this.productRepository.save(product);
@@ -97,7 +99,7 @@ export class ProductsService {
     }
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto, user: User) {
 
     const { images, ...toUpdate } = updateProductDto;
 
@@ -119,6 +121,7 @@ export class ProductsService {
         await queryRunner.manager.delete( ProductImage, { product: { id } } );
         product.images = images.map( image => this.productImageRepository.create({url: image}) )
       }
+      product.user = user;
       await queryRunner.manager.save( product );
 
       await queryRunner.commitTransaction();
